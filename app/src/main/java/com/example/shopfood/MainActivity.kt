746b0f80464cba6bb219c22d4007ff95c637f9a9
MEX1TE <nik.sosin.004@gmail.com>
2025-02-27 00:09:30 +0300

    fix_menu

--- app/src/main/java/com/example/shopfood/MainActivity.kt
+++ app/src/main/java/com/example/shopfood/MainActivity.kt
@@ -10,52 +10,42 @@ import com.example.shopfood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        // Используем View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        changeFragment(HomeFragment())
-
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home ->{
-                    changeFragment(HomeFragment())
-                }
-                R.id.cart ->{
-                    changeFragment(CartFragment())
-                }
-                R.id.search ->{
-                    changeFragment(SearchFragment())
-                }
-                R.id.history ->{
-                    changeFragment(HistoryFragment())
-                }
-                R.id.profile ->{
-                    changeFragment(ProfileFragment())
-                }
-            }
-            return@setOnItemSelectedListener true
-        }
-
+        // Включаем поддержку edge-to-edge
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Устанавливаем стартовый фрагмент
+        changeFragment(HomeFragment())
+
+        // Обрабатываем клики по элементам BottomNavigationView
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> changeFragment(HomeFragment())
+                R.id.cart -> changeFragment(CartFragment())
+                R.id.search -> changeFragment(SearchFragment())
+                R.id.history -> changeFragment(HistoryFragment())
+                R.id.profile -> changeFragment(ProfileFragment())
+            }
+            true
+        }
     }
 
-    fun changeFragment(fragment : Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
-
 }
